04d3f4fd743a47e55be02c49ab4fbc05197c3d8f
Fledik <dima_top2@mail.ru>
2026-07-08 20:13:11 +0300

    Add book model and list endpoint

---
+++ internal/models/book.go
@@ -0,0 +1,16 @@
+package models // Пакет с моделями данных приложения
+
+// Book описывает книгу в нашем API
+type Book struct {
+	ID          int64   `json:"id"`          // Уникальный ID книги
+	ISBN        string  `json:"isbn"`        // Международный номер книги
+	Title       string  `json:"title"`       // Название книги
+	Author      string  `json:"author"`      // Автор книги
+	Description string  `json:"description"` // Краткое описание
+	Publisher   string  `json:"publisher"`   // Издательство
+	Year        int     `json:"year"`        // Год публикации
+	Genre       string  `json:"genre"`       // Жанр книги
+	Language    string  `json:"language"`    // Язык книги
+	Pages       int     `json:"pages"`       // Количество страниц
+	Rating      float64 `json:"rating"`      // Рейтинг книги
+}
